dd9bd1e930caa95ab6d5333722b23c64ea5c8535
tscmoo <the.dengus@gmail.com>
2014-12-19 10:44:02 +0100

    strat_tvp: retreat if they have a few carriers or scouts and we have few goliaths

--- src/strat_tvp.h
+++ src/strat_tvp.h
@@ -43,6 +43,7 @@ struct strat_tvp {
 			if (my_tank_count >= 12) combat::no_aggressive_groups = false;
 			if (enemy_cannon_count >= 3 && enemy_cannon_count >= my_tank_count) combat::no_aggressive_groups = false;
 			if (my_tank_count < 6 && my_tank_count <= (enemy_dragoon_count + enemy_zealot_count) / 2) combat::no_aggressive_groups = true;
+			if ((enemy_carrier_count >= 2 && enemy_scout_count >= 4) && my_goliath_count < 6) combat::no_aggressive_groups = true;
 
 			int desired_science_vessel_count = enemy_arbiter_count;
 			int desired_goliath_count = enemy_carrier_count * 4;
