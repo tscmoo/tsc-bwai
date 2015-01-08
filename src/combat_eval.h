0d55de48107a2ef7fede76e78e5955b95dcbd48a
tscmoo <the.dengus@gmail.com>
2015-01-08 22:56:28 +0100

    combat_eval: assume interceptors can not move. Improves the way we fight carriers.

--- src/combat_eval.h
+++ src/combat_eval.h
@@ -202,7 +202,8 @@ namespace combat_eval {
 							//} else if (target->move > w->max_range) {
 								double speed = c.st->max_speed;
 								if (c.st->type == unit_types::siege_tank_siege_mode) speed = 2;
-								if (c.st->type == unit_types::interceptor && c.move <= 0) speed = 0;
+								//if (c.st->type == unit_types::interceptor && c.move <= 0) speed = 0;
+								if (c.st->type == unit_types::interceptor) speed = 0;
 								if (speed > 0) {
 									++target_count;
 									if (teams[i].has_static_defence) c.move -= c.st->max_speed * frame_resolution / 4;
