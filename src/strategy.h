9ee0bd50859209c2fe1f4bcbcf14c0ce44ae94ec
tscmoo <the.dengus@gmail.com>
2015-01-16 05:51:06 +0100

    strategy: bugfix in update_possible_start_locations, removed some old unused code

--- src/strategy.h
+++ src/strategy.h
@@ -11,7 +11,7 @@ void update_possible_start_locations() {
 	possible_start_locations.clear();
 	for (auto&p : start_locations) {
 		if (p == my_start_location) continue;
-		if (grid::get_build_square(p).last_seen > 30) {
+		if (grid::get_build_square(p).last_seen > 30 && !enemy_buildings.empty()) {
 			double nearest_building = get_best_score_value(enemy_buildings, [&](unit*e) {
 				return diag_distance(e->pos - p);
 			});
@@ -30,8 +30,14 @@ void update_possible_start_locations() {
 		});
 		if (s) {
 			bool found = test_pred(s->resources, [](resource_spots::resource_t&r) {
-				return r.u->resources != r.u->initial_resources;
+				return r.u->resources <= r.u->initial_resources - 8 * 4;
 			});
+			if (found) log("found because of resources\n");
+			auto&bs = grid::get_build_square(s->cc_build_pos);
+			if (bs.building && bs.building->owner == players::opponent_player) {
+				found = true;
+				log("found because of building\n");
+			}
 			if (found) {
 				xy pos = p;
 				possible_start_locations.clear();
@@ -59,163 +65,6 @@ void execute_build(const std::function<bool(buildpred::state&)>&func) {
 	execute_build(false, func);
 }
 
-buildpred::state eval_expand(const std::function<bool(buildpred::state&)>&func) {
-	using namespace buildpred;
- 	double best_score = -std::numeric_limits<double>::infinity();
- 	state best_st;
-	for (int i = 0; i < 2; ++i) {
-		auto initial_state = get_my_current_state();
-		ruleset rules;
-		rules.bases = initial_state.bases.size() + (i ^ 1);
-		if (rules.bases == 1) rules.bases = 2;
-		//int nf = (current_frame + 15 * 60 * 6);
-		//nf -= nf % (15 * 60 * 4);
-		int nf = current_frame + 15 * 60 * 4;
-		int op_nf = nf - 15 * 60 * 2;
-		if (current_frame >= 15 * 60 * 20) op_nf = nf - 15 * 60 * 4;
-		rules.end_frame = nf;
-		rules.func = func;
-
-		a_vector<state> all_states;
-		all_states.push_back(initial_state);
-		run(all_states, rules, true);
-		auto&my_st = all_states.back();
-
-		double score = std::numeric_limits<double>::infinity();
-		for (auto&op_st : run_opponent_builds(op_nf)) {
-			combat_eval::eval eval;
-			eval.max_frames = 15 * 30;
-			for (auto&v : my_st.units) {
-				if (!v.first || !v.second.size()) continue;
-				if (v.first->is_building) continue;
-				if (v.first->is_worker) continue;
-				for (size_t i = 0; i < v.second.size(); ++i) eval.add_unit(units::get_unit_stats(v.first, players::opponent_player), 0);
-			}
-			for (auto&v : op_st.units) {
-				if (!v.first || !v.second.size()) continue;
-				if (v.first->is_building) continue;
-				if (v.first->is_worker) continue;
-				for (size_t i = 0; i < v.second.size(); ++i) eval.add_unit(units::get_unit_stats(v.first, players::my_player), 1);
-			}
-			eval.run();
-			double s = eval.teams[0].end_supply - eval.teams[1].end_supply;
-			if (s < score) score = s;
-		}
-// 		if (score > best_score) {
-// 			best_score = score;
-// 			best_st = std::move(my_st);
-// 		}
-		if (i == 0 && score>0) {
-			log("eval_expand: expand okay with score %g\n", score);
-			return my_st;
-		}
-		if (i == 1) return my_st;
-	}
- 	log("eval_expand: best score is %g\n", best_score);
- 	return best_st;
-}
-
-void execute_build_eval_expand(const std::function<bool(buildpred::state&)>&func) {
-	using namespace buildpred;
-	add_builds(eval_expand(func));
-}
-
-template<typename list_T>
-void execute_best_with_expand(const list_T&list) {
-	using namespace buildpred;
-	log("execute_best_with_expand-\n");
-	for (int i = 0; i < 2; ++i) {
-		double best_score = -std::numeric_limits<double>::infinity();
-		state best_st;
-		for (auto&func : list) {
-			auto initial_state = get_my_current_state();
-			ruleset rules;
-			rules.bases = initial_state.bases.size() + (i ^ 1);
-			if (rules.bases == 1) rules.bases = 2;
-			int nf = current_frame + 15 * 60 * 2;
-			//if (initial_state.army_size >= 50) nf = current_frame + 15 * 60 * 10;
-			//nf = current_frame + 15 * 60 * 20;
-			rules.end_frame = nf;
-			rules.func = func;
-
-			a_vector<state> all_states;
-			all_states.push_back(initial_state);
-			run(all_states, rules, true);
-			auto&my_st = all_states.back();
-
-			double score = std::numeric_limits<double>::infinity();
-			state best_op_st;
-			//for (auto&op_st : run_opponent_builds(nf - 15 * 60 * 2)) {
-			if (true) {
-				auto op_st = get_op_current_state();
-				combat_eval::eval eval;
-				eval.max_frames = 15 * 30;
-				for (auto&v : my_st.units) {
-					if (!v.first || !v.second.size()) continue;
-					if (v.first->is_building) continue;
-					if (v.first->is_worker) continue;
-					unit_type*ut = v.first;
-					if (ut == unit_types::siege_tank_tank_mode) ut = unit_types::siege_tank_siege_mode;
-					int n = (int)std::ceil((double)v.second.size() * 2 - (0.02*(v.second.size()*v.second.size())));
-					for (int i = 0; i < n; ++i) eval.add_unit(units::get_unit_stats(ut, players::opponent_player), 0);
-				}
-				for (auto&v : op_st.units) {
-					if (!v.first || !v.second.size()) continue;
-					if (v.first->is_building) continue;
-					if (v.first->is_worker) continue;
-					unit_type*ut = v.first;
-					if (ut == unit_types::siege_tank_tank_mode) ut = unit_types::siege_tank_siege_mode;
-					int n = (int)std::ceil((double)v.second.size() * 2 - (0.02*(v.second.size()*v.second.size())));
-					for (int i = 0; i < n; ++i) eval.add_unit(units::get_unit_stats(ut, players::my_player), 1);
-				}
-				eval.run();
-				double s = eval.teams[0].end_supply - eval.teams[1].end_supply;
-				//double s = eval.teams[1].start_supply - eval.teams[1].end_supply;
-				//double s = eval.teams[0].score;
-				if (s < score) {
-					score = s;
-					best_op_st = std::move(op_st);
-				}
-			}
-			log("-- i %d - score %g\n", i, score);
-			log("-- my state --- frame %d  min %g gas %g supply %g/%g bases %d\n", my_st.frame, my_st.minerals, my_st.gas, my_st.used_supply[my_st.race], my_st.max_supply[my_st.race], my_st.bases.size());
-			for (auto&v : my_st.units) {
-				if (!v.first || !v.second.size()) continue;
-				log(" %dx%s", v.second.size(), short_type_name(v.first));
-			}
-			log("\n");
-// 			log("produced: \n");
-// 			for (auto&v : my_st.produced) {
-// 				log("  %d: %s\n", v.first, std::get<0>(v.second)->name);
-// 			}
-// 			log("production: \n");
-// 			for (auto&v : my_st.production) {
-// 				log("  %d: %s\n", v.first, v.second->name);
-// 			}
-			log("-- op state --- frame %d  min %g gas %g supply %g/%g bases %d\n", best_op_st.frame, best_op_st.minerals, best_op_st.gas, best_op_st.used_supply[best_op_st.race], best_op_st.max_supply[best_op_st.race], best_op_st.bases.size());
-			for (auto&v : best_op_st.units) {
-				if (!v.first || !v.second.size()) continue;
-				log(" %dx%s", v.second.size(), short_type_name(v.first));
-			}
-			log("\n");
-			if (score > best_score) {
-				best_score = score;
-				best_st = my_st;
-			}
-		}
-		if (i == 0 && best_score > 0) {
-			log("execute_best_with_expand: expand okay with score %g\n", best_score);
-			add_builds(best_st);
-			return;
-		}
-		if (i == 1) {
-			log("execute_best_with_expand: no expand, score %g\n", best_score);
-			add_builds(best_st);
-			return;
-		}
-	}
-
-}
 
 template<typename T>
 struct render_helper {
@@ -259,6 +108,9 @@ std::function<void()> wrap() {
 #include "strat_tvp.h"
 #include "strat_tvz_opening.h"
 #include "strat_tvz.h"
+//#include "strat_fd_push.h"
+//#include "strat_zvp_opening.h"
+//#include "strat_zvp.h"
 
 a_map<a_string, std::function<void()>> strat_map = {
 	{ "proxy rax", wrap<proxy_rax>() },
@@ -269,13 +121,19 @@ a_map<a_string, std::function<void()>> strat_map = {
 	{ "tvp", wrap<strat_tvp>() },
 	{ "tvz opening", wrap<strat_tvz_opening>() },
 	{ "tvz", wrap<strat_tvz>() },
+//	{ "fd push", wrap<strat_fd_push>() },
+//	{ "zvp opening", wrap<strat_zvp_opening>() },
+//	{ "zvp", wrap<strat_zvp>() },
 };
 
 bool run_strat(const char*name) {
 	try {
+		log("running strat %s\n", name);
 		strat_map[name]();
+		log("strat %s done\n", name);
 		return true;
 	} catch (strat_failed&) {
+		log("strat %s failed\n", name);
 		return false;
 	}
 }
@@ -284,22 +142,32 @@ void strategy_task() {
 
 	multitasking::sleep(1);
 
-	if (players::opponent_player->random) {
-		run_strat("tvp opening");
-	} else if (players::opponent_player->race == race_terran) {
-		run_strat("tvt opening");
-	} else if (players::opponent_player->race == race_protoss) {
-		run_strat("tvp opening");
-	} else if (players::opponent_player->race == race_zerg) {
-		run_strat("tvz opening");
-	}
+	if (players::my_player->race == race_terran) {
 
-	if (players::opponent_player->race == race_terran) {
-		run_strat("tvt");
-	} else if (players::opponent_player->race == race_protoss) {
-		run_strat("tvp");
-	} else if (players::opponent_player->race == race_zerg) {
-		run_strat("tvz");
+		if (players::opponent_player->random) {
+			run_strat("tvp opening");
+		} else if (players::opponent_player->race == race_terran) {
+			run_strat("tvt opening");
+		} else if (players::opponent_player->race == race_protoss) {
+			run_strat("tvp opening");
+			//run_strat("fd push");
+		} else if (players::opponent_player->race == race_zerg) {
+			run_strat("tvz opening");
+		}
+
+		if (players::opponent_player->race == race_terran) {
+			run_strat("tvt");
+		} else if (players::opponent_player->race == race_protoss) {
+			run_strat("tvp");
+		} else if (players::opponent_player->race == race_zerg) {
+			run_strat("tvz");
+		}
+
+	} else if (players::my_player->race == race_protoss) {
+
+	} else if (players::my_player->race == race_zerg) {
+		run_strat("zvp opening");
+		run_strat("zvp");
 	}
 
 	while (true) {
