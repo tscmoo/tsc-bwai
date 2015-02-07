c7dab03fe4caad2bec7c745fd25c147719b9d0df
tscmoo <the.dengus@gmail.com>
2015-02-07 11:46:47 +0100

    render: only draw the income text if test_mode is set

--- src/render.h
+++ src/render.h
@@ -118,7 +118,8 @@ void render() {
 	avg_load /= avg_load_vec.size();
 
 	draw_screen_stacked_text(16,8,format("FPS: %.02f  load: %02d%%  carry: %.02f   APM: %d",fps,(int)(avg_load*100),-multitasking::detail::frame_time_carry,game->getAPM()));
-	draw_screen_stacked_text(16,8,format("%02d:%02d  Income: %.02f/%.02f  Predicted: %.02f/%.02f",current_frame/15/60,current_frame/15%60,current_minerals_per_frame,current_gas_per_frame,predicted_minerals_per_frame,predicted_gas_per_frame));
+	if (test_mode) draw_screen_stacked_text(16,8,format("%02d:%02d  Income: %.02f/%.02f  Predicted: %.02f/%.02f",current_frame/15/60,current_frame/15%60,current_minerals_per_frame,current_gas_per_frame,predicted_minerals_per_frame,predicted_gas_per_frame));
+	else draw_screen_stacked_text(16, 8, format("%02d:%02d", current_frame / 15 / 60, current_frame / 15 % 60));
 
 	if (test_mode) {
 		render_task_list();
