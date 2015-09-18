9e7d804a22ecaa9ebe1e4c826e9858829ba14d7a
tscmoo <the.dengus@gmail.com>
2015-09-18 05:25:46 +0200

    strat_z_10hatch: modified to deal with rushes (or zvz) slightly better

--- src/strat_z_10hatch.h
+++ src/strat_z_10hatch.h
@@ -28,27 +28,40 @@ struct strat_z_10hatch : strat_z_base {
 			build::add_build_task(1.0, unit_types::spawning_pool);
 			build::add_build_task(2.0, unit_types::drone);
 			build::add_build_task(3.0, unit_types::overlord);
-			++opening_state;
-		} else if (opening_state == 3) {
-			//bo_gas_trick();
-			if (bo_all_done()) {
-				scouting::scout_supply = 10.0;
-				build::add_build_task(3.0, unit_types::drone);
+			build::add_build_task(4.0, unit_types::drone);
+			if (players::opponent_player->race == race_zerg || players::opponent_player->random) {
+				build::add_build_task(5.0, unit_types::zergling);
+				build::add_build_task(5.0, unit_types::zergling);
+				build::add_build_task(5.0, unit_types::zergling);
+			} else {
 				build::add_build_task(4.0, unit_types::drone);
-				build::add_build_task(5.0, unit_types::drone);
-				++opening_state;
+				build::add_build_task(4.0, unit_types::drone);
+				build::add_build_task(5.0, unit_types::zergling);
+				build::add_build_task(5.0, unit_types::zergling);
 			}
-		} else if (opening_state == 4) {
-			if (bo_all_done()) {
-				build::add_build_task(0.0, unit_types::zergling);
-				build::add_build_task(0.0, unit_types::zergling);
-				++opening_state;
-			}
+			++opening_state;
+// 		} else if (opening_state == 3) {
+// 			//bo_gas_trick();
+// 			if (bo_all_done()) {
+// 				scouting::scout_supply = 10.0;
+// 				build::add_build_task(3.0, unit_types::drone);
+// 				build::add_build_task(4.0, unit_types::drone);
+// 				build::add_build_task(5.0, unit_types::drone);
+// 				++opening_state;
+// 			}
+// 		} else if (opening_state == 4) {
+// 			if (bo_all_done()) {
+// 				build::add_build_task(0.0, unit_types::zergling);
+// 				build::add_build_task(0.0, unit_types::zergling);
+// 				++opening_state;
+// 			}
 		} else if (opening_state != -1) {
 			if (bo_all_done()) {
 				opening_state = -1;
 			}
 		}
+
+		if (enemy_proxy_building_count && army_supply == 0.0) max_bases = 1;
 
 		return opening_state == -1;
 	}
