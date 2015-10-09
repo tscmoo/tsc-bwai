5b5d9ddf5b1e1ccf6f04974562c168260105a9bd
tscmoo <the.dengus@gmail.com>
2015-10-09 06:49:15 +0200

    strat_z_9pool: use add_build_total

--- src/strat_z_9pool.h
+++ src/strat_z_9pool.h
@@ -12,13 +12,9 @@ struct strat_z_9pool: strat_z_base {
 	}
 	virtual bool tick() override {
 		if (opening_state == 0) {
-			if (my_resource_depots.size() != 1 || my_workers.size() != 4) opening_state = -1;
+			if (my_resource_depots.size() != 1 || my_workers.size() > 9) opening_state = -1;
 			else {
-				build::add_build_task(0.0, unit_types::drone);
-				build::add_build_task(0.0, unit_types::drone);
-				build::add_build_task(0.0, unit_types::drone);
-				build::add_build_task(0.0, unit_types::drone);
-				build::add_build_task(0.0, unit_types::drone);
+				build::add_build_total(0.0, unit_types::drone, 9);
 				build::add_build_task(1.0, unit_types::spawning_pool);
 				build::add_build_task(2.0, unit_types::drone);
 				++opening_state;
@@ -26,7 +22,7 @@ struct strat_z_9pool: strat_z_base {
 		} else if (opening_state == 1) {
 			bo_gas_trick();
 		} else if (opening_state == 2) {
-			build::add_build_task(0.0, unit_types::overlord);
+			build::add_build_total(0.0, unit_types::overlord, 2);
 			build::add_build_task(1.0, unit_types::zergling);
 			build::add_build_task(1.0, unit_types::zergling);
 			build::add_build_task(1.0, unit_types::zergling);
